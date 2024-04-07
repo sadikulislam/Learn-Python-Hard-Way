d34861f34a657b15d8686238849be8892f30a575
Sadikul Islam <sadikulislam086@gmail.com>
2024-04-07 18:54:16 +0600

    added exercise 3

---
+++ exe03.py
@@ -0,0 +1,20 @@
+# Number and Math
+print(" I will now count my chickens: ")
+
+print("Hens", 25 + 10 / 2)
+print("Rooster", 100 - 25 * 3 % 7)
+
+print("Count eggs: ")
+print(5 + 2 + 1 - 2 + 6 % 2 -1 / 4 + 7)
+
+print("Is that true  5 + 9 < 12 - 19?")
+print(5 + 9 < 12 - 19)
+
+print("What is 5 + 9?", 5 + 9)
+print("What is 12 - 19?", 12 - 19)
+
+print("Oh, that's why it's False.")
+
+print("Is it greater?", 9.1 > -7.0)
+print("Is it greater or equal?", 9 >= -7)
+print("Is it less or equal?", 9 <= -7)
