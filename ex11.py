e4bd1f4a283b7377f59a87e263a633e8f707a944
Sadikul Islam <sadikulislam086@gmail.com>
2024-04-08 02:48:26 +0600

    added exercise 11

---
+++ ex11.py
@@ -0,0 +1,14 @@
+#  get any input from a person
+
+print("How old are you?", end=' ')
+age = input()
+# print("How tall are you?", end=' ')
+# height = input()
+# print("How much do you weigh?", end=' ')
+# weight = input()
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
